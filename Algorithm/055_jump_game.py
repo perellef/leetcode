2bcc8b0496013294aa2c3055d359800ae5d32760
UnusuaI <perellef2000@gmail.com>
2026-01-13 14:46:22 +0100

    Nye løsninger

---
+++ Algorithm/055_jump_game.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/spiral-matrix/description/
+
+nums = [3,2,1,0,4]
+
+def løsning(ns):
+    j = len(ns)-1
+    for i in range(len(ns)-2,-1,-1):
+        if ns[i] >= j-i:
+            j = i
+    return j == 0
+
+print(løsning(nums))
